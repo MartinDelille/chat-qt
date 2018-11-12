da23785181f972e83c568285492c2e5501eed74d
Martin Delille <martin@phonations.com>
2018-11-12 22:16:26 +0100

    Filter message

--- ConversationModel.cpp
+++ ConversationModel.cpp
@@ -1,11 +1,13 @@
+#include <QDebug>
+
 #include "ConversationModel.h"
 
 ConversationModel::ConversationModel(QObject *parent) : QAbstractListModel(parent)
 {
 	_messages.append({"Albert Einstein", "Me", QDateTime::fromString("2015-11-20T06:30:02", Qt::ISODate), "Coucou"});
 	_messages.append({"Me", "Albert Einstein", QDateTime::fromString("2015-11-20T06:30:12", Qt::ISODate), "Ca va?"});
-	_messages.append({"Ernest Hemingway", "Me", QDateTime::fromString("2015-11-20T06:30:02", Qt::ISODate), "Coucou"});
-	_messages.append({"Me", "Ernest Hemingway", QDateTime::fromString("2015-11-20T06:30:12", Qt::ISODate), "Ca va?"});
+	_messages.append({"Ernest Hemingway", "Me", QDateTime::fromString("2015-11-20T06:30:02", Qt::ISODate), "Hello"});
+	_messages.append({"Me", "Ernest Hemingway", QDateTime::fromString("2015-11-20T06:30:12", Qt::ISODate), "Good?"});
 }
 
 QString ConversationModel::recipient() const
@@ -15,22 +17,35 @@ QString ConversationModel::recipient() const
 
 int ConversationModel::rowCount(const QModelIndex &) const
 {
-	return _messages.count();
+	int count = 0;
+	foreach(auto message, _messages) {
+		if (recipient() == message.author || recipient() == message.recipient) {
+			count++;
+		}
+	}
+	return count;
 }
 
 QVariant ConversationModel::data(const QModelIndex &index, int role) const
 {
-	if (index.row() < _messages.count()) {
-		Message message = _messages[index.row()];
-		switch (role) {
-		case AuthorRole:
-			return message.author;
-		case RecipientRole:
-			return message.recipient;
-		case TimestampRole:
-			return message.timestamp.toString(Qt::ISODate);
-		case ContentRole:
-			return message.content;
+	int i = 0;
+
+	foreach(auto message, _messages) {
+		if (recipient() == message.author || recipient() == message.recipient) {
+			if (i == index.row()) {
+				switch (role) {
+				case AuthorRole:
+					return message.author;
+				case RecipientRole:
+					return message.recipient;
+				case TimestampRole:
+					return message.timestamp.toString(Qt::ISODate);
+				case ContentRole:
+					qDebug() << message.content;
+					return message.content;
+				}
+			}
+			i++;
 		}
 	}
 	return QVariant();
